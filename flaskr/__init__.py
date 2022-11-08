25749025f52d29f8e84224e1930be4d666e031cc
stigsanek <stigsanek@yandex.ru>
2022-11-08 11:46:19 +0400

    Change quotes

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -12,13 +12,13 @@ def create_app(test_config: dict = None) -> Flask:
     """
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite')
+        SECRET_KEY="dev",
+        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite")
     )
 
     if test_config is None:
         # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
+        app.config.from_pyfile("config.py", silent=True)
     else:
         app.config.from_mapping(test_config)
 
